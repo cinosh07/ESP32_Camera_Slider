d9f1d3353c27adf08f1803839bba34d3762d254f
Carl Tremblay <cinosh07@hotmail.com>
2023-02-15 21:49:39 -0500

    cleanup

--- firmware/output/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-d:\Timelapse_Slider\ESP32_Camera_Slider\firmware\output\core\libb64\cdecode.c.o: \
- C:\Users\Carl\Documents\ArduinoData\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cdecode.c \
- C:\Users\Carl\Documents\ArduinoData\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cdecode.h \
- C:\Users\Carl\Documents\ArduinoData\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/stdint.h \
- C:\Users\Carl\Documents\ArduinoData\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\Carl\Documents\ArduinoData\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/sys/features.h \
- C:\Users\Carl\Documents\ArduinoData\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/sys/_intsup.h
--- firmware/output/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-d:\Timelapse_Slider\ESP32_Camera_Slider\firmware\output\core\libb64\cencode.c.o: \
- C:\Users\Carl\Documents\ArduinoData\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cencode.c \
- C:\Users\Carl\Documents\ArduinoData\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cencode.h
